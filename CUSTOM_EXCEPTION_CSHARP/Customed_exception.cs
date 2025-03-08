db4a6dcfdc8f32e27d92b069770c2f67e71d494d
priyal <21h51a7364@cmrcet.ac.in>
2025-03-08 14:51:36 +0530

    Custom exception handling

---
+++ CUSTOM_EXCEPTION_CSHARP/Customed_exception.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUSTOM_EXCEPTION_CSHARP
+{
+    class Customed_exception:Exception
+    {
+        public override string Message => $"{DateTime.Now.ToString()} - Internal Exception at execuation";
+    }
+}
---
+++ CUSTOM_EXCEPTION_CSHARP/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CUSTOM_EXCEPTION_CSHARP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            try
+            {
+                var res = Anyfunc(0);
+
+            }
+            catch(Customed_exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static int Anyfunc(int n)
+        {
+            if(n==0)
+            {
+                throw new Customed_exception();
+            }
+            return n * 2;
+        }
+    }
+}
