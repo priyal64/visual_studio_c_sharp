08e600c2304d3b1ee03dc142fd9fdf55f443a8fc
priyal <21h51a7364@cmrcet.ac.in>
2025-03-08 14:47:21 +0530

    exception handling

---
+++ Exception_hNDLING_IN_CSHARP/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Exception_hNDLING_IN_CSHARP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] a = new int[10];
+                int j = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    a[i] = j++;
+                }
+
+                a[10] = j++; // IndexOutOfRangeException
+
+                // Uncomment the following line to cause a FormatException
+                // a[9] = int.Parse("String i want to give");
+
+                //Uncomment the following line to cause a NullReferenceException
+                //string nullString = null;
+                //int length = nullString.Length;
+
+                //Uncomment the following line to cause an ArithmeticException
+                //int zero = 0;
+                //int result = 10 / zero;
+
+                //Uncomment the following lines to cause a StackOverflowException (be careful, this can crash your application)
+                //RecursiveMethod();
+
+                //Uncomment to cause an array type mismatch
+                //object[] objArray = new object[1];
+                //objArray[0] = "string";
+                //int x = (int)objArray[0];
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: Array index is out of bounds.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: Invalid format.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine("Error: Arithmetic operation failed.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Error: Null reference exception.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (StackOverflowException ex)
+            {
+                Console.WriteLine("Error: Stack overflow exception.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine("Error: Invalid Cast Exception");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occurred.");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Execution complete (try-catch-finally block).");
+            }
+        }
+
+        //Recursive method to cause a StackOverflowException
+        static void RecursiveMethod()
+        {
+            RecursiveMethod();
+        }
+    }
+}
---
+++ Practice_m2_csharp/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Practice_m2_csharp
+{
+    internal class Program
+    {
+        static int find_min(int[] num,int n)
+        {
+            int ans = Int32.MaxValue;
+            for(int i=0;i<n;i++)
+            {
+                if (ans > num[i])
+                {
+                    ans = num[i];
+                }
+            }
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            //string str = "wwwowww";
+            //if(Palindrome(str))
+            //{
+            //    Console.WriteLine("yes it is palindrome");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No it is not palindrome");
+            //}
+            Console.WriteLine("Enter length value");
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("enter value for index " + i);
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            int ans = find_min(num,n);
+            Console.WriteLine(" the minimum number is " + ans);
+        }
+
+        static string Reverse(string str)
+        {
+            char[] charray = str.ToCharArray();
+            int len = charray.Length;
+            string rev = "";
+            for (int i=0;i<len;i++)
+            {
+                rev += charray[len - i - 1];
+            }
+            return rev;
+        }
+        static bool Palindrome(string str)
+        {
+            string rev = Reverse(str);
+            return str.Equals(rev);
+        }
+    }
+}
