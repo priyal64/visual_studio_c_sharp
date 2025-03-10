9744deb32b8a4676e16e82e749ecd74d0c74e2df
priyal <21h51a7364@cmrcet.ac.in>
2025-03-10 09:25:36 +0530

    ASYNC AND AWAIT

---
+++ Async_await/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Async_await
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Fetching data...{DateTime.Now.ToString()}");
+            //m1 4
+            //m2
+            string result = await FetchDataAsync("https://Google.com");//3
+
+            Console.WriteLine(result);
+        }
+        public static async Task<string> FetchDataAsync(string url)
+        {
+            Console.WriteLine($"I am working {DateTime.Now.ToString()} ");
+            using (HttpClient client = new HttpClient())
+            {
+                // The await keyword allows the method to be non-blocking
+                string response = await client.GetStringAsync(url);
+                Console.WriteLine("I am done");
+                return response;
+            }
+
+        }
+
+    }
+}
